83413f3c5687e2ddcf4e54fffa27cdce1e27344d
carsonjensensmith35 <carsonjensensmith35@gmail.com>
2023-11-29 19:39:19 -0700

    Added React MUI, TypeScript React Starter Project with a button component showing

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ButtonComponent from './components/ButtonComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1> My First React Application From Scratch</h1>
+      <ButtonComponent/> 
+    </div>
+  );
+}
+
+export default App;
