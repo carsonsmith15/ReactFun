eac0dd56918e7269d8ed0afae84dd6322dfd3fe7
carsonjensensmith35 <carsonjensensmith35@gmail.com>
2023-11-29 19:57:36 -0700

    Deleting the logo image in the manifest file to get the error to go away

--- my-app/src/components/ButtonComponent.tsx
+++ my-app/src/components/ButtonComponent.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import  Button  from '@mui/material/Button'
 
+const handleClick = (event: React.MouseEvent<HTMLElement>, text: string) => {
+  console.log(event.target);
+  console.log(text);
+};
+
 function ButtonComponent() {
   return (
     <div>
-      <Button variant='contained'>Hello World</Button>
+      <Button 
+        variant='contained'
+        
+        onClick={(e) => handleClick(e, "clicked")}>Hello .World</Button>
     </div>
   )
 }
